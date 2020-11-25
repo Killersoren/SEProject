66a31cc9f987f60b37768cd087353b4d45962cf6
Hansatang <portokarlos@gmail.com>
2020-11-25 12:08:57 +0100

    Pacierz 25/11/2020

--- src/Controller.java
+++ src/Controller.java
@@ -1,13 +1,15 @@
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class Controller
 {
@@ -35,8 +37,57 @@ public class Controller
     }
   }
 
-  @FXML public void addProjectClick()
+  @FXML public void addEmployeeClick()
   {
+    Stage window = new Stage();
 
+    window.initModality(Modality.APPLICATION_MODAL);
+    window.setTitle("Add new project");
+    window.setMinWidth(300);
+
+    // Member name input.
+    HBox nameContainer = new HBox(2);
+    nameContainer.setPadding(new Insets(10, 10, 0, 10));
+    Label memberName = new Label("Member name: ");
+    TextField inputmemberName = new TextField();
+    inputmemberName.setPromptText("Enter member name");
+    nameContainer.getChildren().addAll(memberName, inputmemberName);
+
+    Label errorMessage = new Label("");
+
+    Button closeButton = new Button("Add new project");
+
+    closeButton.setOnAction(new EventHandler<ActionEvent>()
+    {
+      @Override public void handle(ActionEvent e)
+      {
+        if (!(inputmemberName.getText().isEmpty() || inputmemberName.getText()
+            .equals("")))
+        {
+          window.close();
+          Project project = new Project(inputmemberName.getText());
+          addProjectToList(project);
+          Main.projects.add(project);
+          setCurrentProject(project);
+        }
+        else
+        {
+          errorMessage.setText("ERROR: invalid project name");
+          errorMessage.setTextFill(Color.RED);
+        }
+      }
+    });
+
+    VBox layout = new VBox(10);
+
+    layout.getChildren().addAll(nameContainer, errorMessage, closeButton);
+    layout.setAlignment(Pos.CENTER);
+
+    Scene scene = new Scene(layout);
+    window.setResizable(false);
+    window.setScene(scene);
+    window.showAndWait();
+
+  }
   }
 }
