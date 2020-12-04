4c7d03a29aea71a405b8d29efb82e6c7d8a8676b
Esben F <304568@via.dk>
2020-12-04 14:45:35 +0100

    Package folders

--- src/Requirement.java
+++ src/Requirement.java
@@ -1,19 +1,22 @@
 import java.io.Serializable;
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Requirement implements Serializable
 {
   private String name,userstory,status;
   private int estimatedHours,totalHoursWorked;
+  private LocalDate deadline;
   private ArrayList<Task> tasks;
 
   public Requirement(String name, String userstory, String status,
-      int estimatedHours, int totalHoursWorked,
+      LocalDate deadline, int estimatedHours, int totalHoursWorked,
        ArrayList<Task> tasks) {
     this.name = name;
     this.userstory = userstory;
     this.status = status;
     this.estimatedHours = estimatedHours;
+    this.deadline = deadline;
     this.totalHoursWorked = totalHoursWorked;
     this.tasks = tasks;
   }
