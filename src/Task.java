9e5bf35450b7df326b41981124c6545c0c5d7e48
Esben F <304568@via.dk>
2020-12-04 14:47:15 +0100

    Package folders

--- src/Task.java
+++ src/Task.java
@@ -1,16 +1,19 @@
 import java.io.Serializable;
+import java.time.LocalDate;
 
 public class Task implements Serializable
 {
   private String name,status;
   private int relatedRequirement,taskID, estimatedHours,totalHoursWorked;
+  private LocalDate deadline;
   private Member responsibleMember;
   private  MemberList taskMembers;
 
   public Task(String name, String status, int relatedRequirement, int taskID,
-      int estimatedHours, int totalHoursWorked, Member responsibleMember,
+      int estimatedHours, int totalHoursWorked, Member responsibleMember, LocalDate deadline,
       MemberList taskMembers)
   {
+    this.deadline = deadline;
     this.name = name;
     this.status = status;
     this.relatedRequirement = relatedRequirement;
